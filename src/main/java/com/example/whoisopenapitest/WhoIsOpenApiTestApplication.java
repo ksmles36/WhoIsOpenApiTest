53e5096ef9fbde2d60e617309f985da000d98e33
ksm <ksmles36@imokorea.co.kr>
2023-11-11 22:52:04 +0900

    Initial commit

---
+++ src/main/java/com/example/whoisopenapitest/WhoIsOpenApiTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.whoisopenapitest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoIsOpenApiTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhoIsOpenApiTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/whoisopenapitest/WhoIsOpenApiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whoisopenapitest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhoIsOpenApiTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
